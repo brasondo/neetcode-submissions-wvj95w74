6307d559513d1faa1de6458bd3b32eee5468502a
Brason Dobson <110146760+brasondo@users.noreply.github.com>
2026-04-17 14:10:11 -0400

    Add: is-anagram - submission-25

---
+++ Data Structures & Algorithms/is-anagram/submission-25.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+
+        if len(s) != len(t):
+            return False
+        
+        countS, countT = Counter(s), Counter(t)
+
+        if countS != countT:
+            return False
+        return True
+        
+        
