1ca13e68ff845412ad97a8c1281364d195ed12b7
Brason Dobson <110146760+brasondo@users.noreply.github.com>
2026-05-07 19:57:22 -0400

    Add: is-anagram - submission-27

---
+++ Data Structures & Algorithms/is-anagram/submission-27.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+
+        countS = Counter(s)
+        countT= Counter(t)
+
+        if countS == countT:
+            return True
+        else:
+            return False
+
+
+
+
+        
